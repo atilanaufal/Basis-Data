f60973f80a85667cc2902b7e30bc23aa4d87ac3f
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-20 22:28:11 +0700

    Update DDL.sql

--- Pertemuan 16 (UAS)/DDL.sql
+++ Pertemuan 16 (UAS)/DDL.sql
@@ -89,3 +89,17 @@ CREATE TABLE IF NOT EXISTS riwayat_kesehatan (
     updated_at DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     FOREIGN KEY (id_anak) REFERENCES data_anak(id_anak) ON DELETE CASCADE
 );
+
+
+-- Tabel Audit Logs
+CREATE TABLE IF NOT EXISTS audit_logs (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    table_name VARCHAR(255),
+    record_id INT,
+    operation_type ENUM('INSERT', 'UPDATE', 'DELETE'),
+    changed_by INT,
+    role ENUM('admin', 'user'),
+    changed_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+    old_data JSON,
+    new_data JSON
+);
