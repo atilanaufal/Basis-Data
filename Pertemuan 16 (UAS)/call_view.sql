64d49e8bf8f626e5fe51d0482b2dd48d20c15808
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-20 22:37:50 +0700

    Create call_view.sql

---
+++ Pertemuan 16 (UAS)/call_view.sql
@@ -0,0 +1,14 @@
+-- Memanggil view riwayat_kesehatan_lengkap
+SELECT * FROM riwayat_kesehatan_lengkap;
+
+-- Memanggil view jadwal_harian_anak
+SELECT * FROM jadwal_harian_anak;
+
+-- Memanggil view data_anak_lengkap
+SELECT * FROM data_anak_lengkap;
+
+-- Memanggil view aktivitas_user
+SELECT * FROM aktivitas_user;
+
+-- Memanggil view aktivitas_admin
+SELECT * FROM aktivitas_admin;
