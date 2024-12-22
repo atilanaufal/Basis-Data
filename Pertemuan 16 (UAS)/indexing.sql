d9d71b755cdeaae7a722604539ea5b0803c9b5db
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-22 19:03:39 +0700

    Update indexing.sql

--- Pertemuan 16 (UAS)/indexing.sql
+++ Pertemuan 16 (UAS)/indexing.sql
@@ -1,9 +1,8 @@
-
 CREATE INDEX idx_ripe_user ON akun_user (tipe_user);
 
 CREATE INDEX idx_umur ON data_anak (umur);
 
-CREATE INDEX idx_menu_sarapan ON jadwal_makan (menu_sarapan);
+CREATE INDEX idx_menu_sarapan ON jadwal_makan (menu_sarapan(100));
 
 CREATE INDEX idx_waktu_bangun ON jadwal_tidur (waktu_bangun);
 
