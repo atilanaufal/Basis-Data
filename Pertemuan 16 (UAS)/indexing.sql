bb66d33245b4c9df1a19722ad33877b1d48fa3c5
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-22 19:00:10 +0700

    Update indexing.sql

--- Pertemuan 16 (UAS)/indexing.sql
+++ Pertemuan 16 (UAS)/indexing.sql
@@ -1,17 +1,12 @@
--- Indeks untuk kolom id_user pada tabel data_anak
-CREATE INDEX idx_id_user ON data_anak (id_user);
 
--- Indeks untuk kolom nama_anak pada tabel data_anak
-CREATE INDEX idx_nama_anak ON data_anak (nama_anak);
+CREATE INDEX idx_ripe_user ON akun_user (tipe_user);
 
--- Indeks untuk kolom id_anak pada tabel jadwal_makan
-CREATE INDEX idx_id_anak_makan ON jadwal_makan (id_anak);
+CREATE INDEX idx_umur ON data_anak (umur);
 
--- Indeks untuk kolom id_anak pada tabel jadwal_tidur
-CREATE INDEX idx_id_anak_tidur ON jadwal_tidur (id_anak);
+CREATE INDEX idx_menu_sarapan ON jadwal_makan (menu_sarapan);
 
--- Indeks untuk kolom id_anak pada tabel kegiatan_anak
-CREATE INDEX idx_id_anak_kegiatan ON kegiatan_anak (id_anak);
+CREATE INDEX idx_waktu_bangun ON jadwal_tidur (waktu_bangun);
 
--- Indeks untuk kolom nama_penyakit pada tabel riwayat_kesehatan
+CREATE INDEX idx_nama_kegiatan ON kegiatan_anak (nama_kegiatan);
+
 CREATE INDEX idx_nama_penyakit ON riwayat_kesehatan (nama_penyakit);
