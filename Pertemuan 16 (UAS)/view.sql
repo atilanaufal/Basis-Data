442ed0a50266abc0c7cc2774d2bb59824c34abe8
CorSS_REST <39077480+atilanaufal@users.noreply.github.com>
2024-12-22 11:01:37 +0700

    Update view.sql

--- Pertemuan 16 (UAS)/view.sql
+++ Pertemuan 16 (UAS)/view.sql
@@ -1,3 +1,4 @@
+-- Data Anak Lengkap View --
 CREATE OR REPLACE VIEW data_anak_lengkap AS
 SELECT
     au.id_user,
@@ -12,6 +13,7 @@ SELECT
 FROM data_anak da
 JOIN akun_user au ON da.id_user = au.id_user;
 
+-- Riwayat Kesehatan Lengkap View --
 CREATE OR REPLACE VIEW riwayat_kesehatan_lengkap AS
 SELECT 
     rk.id_kesehatan,
@@ -26,6 +28,7 @@ FROM riwayat_kesehatan rk
 JOIN data_anak da ON rk.id_anak = da.id_anak
 JOIN akun_user au ON da.id_user = au.id_user;
 
+-- Jadwal Harian Anak View --
 CREATE OR REPLACE VIEW jadwal_harian_anak AS
 SELECT 
     da.id_anak,
@@ -46,6 +49,7 @@ LEFT JOIN jadwal_makan jm ON da.id_anak = jm.id_anak
 LEFT JOIN jadwal_tidur jt ON da.id_anak = jt.id_anak
 LEFT JOIN kegiatan_anak ka ON da.id_anak = ka.id_anak;
 
+-- Aktivitas User View --
 CREATE OR REPLACE VIEW aktivitas_user AS
 SELECT
     al.changed_at,
@@ -58,6 +62,7 @@ FROM audit_logs al
 JOIN akun_user au ON al.changed_by = au.id_user
 WHERE al.role = 'user';
 
+-- Aktivitas Admin View --
 CREATE OR REPLACE VIEW aktivitas_admin AS
 SELECT
     al.changed_at,
@@ -69,3 +74,27 @@ SELECT
 FROM audit_logs al
 JOIN akun_admin aa ON al.changed_by = aa.id_admin
 WHERE al.role = 'admin';
+
+-- User List View --
+CREATE VIEW user_list AS
+SELECT
+    a.id_admin,
+    a.nama_admin,
+    a.email,
+    'admin' AS role,
+    'Semua hak akses' AS hak_akses
+FROM
+    akun_admin a
+UNION ALL
+SELECT 
+    u.id_user,
+    u.nama_user,
+    u.email,
+    u.tipe_user AS role,
+    CASE
+        WHEN u.tipe_user = 'orang_tua' THEN 'SELECT, INSERT, UPDATE, DELETE pada data anak, jadwal makan, jadwal tidur, kegiatan anak, dan riwayat kesehatan'
+        WHEN u.tipe_user = 'pengasuh' THEN 'SELECT pada data anak, SELECT, UPDATE pada jadwal makan, jadwal tidur, kegiatan anak, dan riwayat kesehatan'
+        ELSE 'Tidak ada hak akses'
+    END AS hak_akses
+FROM 
+    akun_user u;
